38d9b3e4a788c2660dc0b018530acef9861667c8
MelinaMontes <you@example.com>
2021-06-13 18:52:27 -0300

    first commit

---
+++ src/main/java/ar/com/ada/api/apicreditos/ApicreditosApplication.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.apicreditos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicreditosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicreditosApplication.class, args);
+	}
+
+}
