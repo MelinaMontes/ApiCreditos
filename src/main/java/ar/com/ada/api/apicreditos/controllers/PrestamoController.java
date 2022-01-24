4c381292f39ff024e84e998417d7a043342b544d
MelinaMontes <you@example.com>
2022-01-24 16:07:21 -0300

    agrego anotacion faltante en la clase prestamo para que al realizar la peticion al servidor me devuelva la lista de  todos los clientes

--- src/main/java/ar/com/ada/api/apicreditos/controllers/PrestamoController.java
+++ src/main/java/ar/com/ada/api/apicreditos/controllers/PrestamoController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import ar.com.ada.api.apicreditos.entities.Cliente;
 import ar.com.ada.api.apicreditos.entities.Prestamo;
 import ar.com.ada.api.apicreditos.models.GenericResponse;
 import ar.com.ada.api.apicreditos.services.ClienteService;
@@ -20,11 +19,11 @@ import ar.com.ada.api.apicreditos.services.PrestamoService;
 public class PrestamoController {
     @Autowired
     PrestamoService service;
+
     @GetMapping("/prestamos")
     public ResponseEntity<List<Prestamo>> traerTodos() {
         return ResponseEntity.ok(service.traerTodos());
 
     }
-
 
 }
